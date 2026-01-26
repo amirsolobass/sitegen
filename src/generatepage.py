0596d3d16492350e0cb730a41c183db847b4d817
amirsolobass <amirsolobass@gmail.com>
2026-01-26 18:51:45 +0100

    recursive page gen

--- src/generatepage.py
+++ src/generatepage.py
@@ -5,7 +5,7 @@ import os
 
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath="/"):
     if not os.path.exists(from_path):
         raise FileNotFoundError(f"Source markdown file '{from_path}' does not exist.")
     if not os.path.isfile(from_path):
@@ -27,12 +27,15 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(content)
     # replace {{content}} and {{title}} in template
     final_html = temp.replace("{{ Title }}", title).replace("{{ Content }}", html_content)
+    # replace href and src paths to be relative
+    final_html = final_html.replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
+
     # write to dest_path
 
     open(dest_path, "w").write(final_html)
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath="/"):
     root = os.path.abspath(dir_path_content)
     for dirpath, dirnames, filenames in os.walk(root):
         for filename in filenames:
@@ -41,3 +44,8 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
                 relative_path = os.path.relpath(from_path, root)
                 dest_path = os.path.join(dest_dir_path, relative_path[:-3] + ".html")  # change .md to .html
                 generate_page(from_path, template_path, dest_path)
+    print("ROOT:", root)
+    for dirpath, dirnames, filenames in os.walk(root):
+        print("DIR:", dirpath)
+        for filename in filenames:
+            print("FILE:", filename)
--- src/main.py
+++ src/main.py
@@ -1,16 +1,21 @@
 from inline_markdown import extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link
 from copystatic import copy_from_dir
 from generatepage import generate_pages_recursive
-
-
-    
-
+import sys
 
 
 def main():
-    copy_from_dir("static", "public")
-    print("Copied static files from 'static' to 'public' directory.")
-    generate_pages_recursive("content", "template.html", "public")
+    basepath = "/"
+    if sys.argv and len(sys.argv) > 1:
+        basepath = sys.argv[1]
+
+
+
+
+    copy_from_dir("static", "docs")
+    print("Copied static files from 'static' to 'docs' directory.")
+    print(f"Basepath for links set to: {basepath}")
+    generate_pages_recursive("content", "template.html", "docs", basepath)
     print("Generated pages recursively from 'content' using 'template.html'.")
     
 
